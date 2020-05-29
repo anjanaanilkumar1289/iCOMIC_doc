03fb0b95932e343ae12a015d2cb19acf7d544fb3
Philge Philip <philgev2@gmail.com>
2020-05-29 12:36:38 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,9 @@ from recommonmark.parser import CommonMarkParser
 source_parsers = {
     '.md': CommonMarkParser,
 }
+extensions = [
+    'sphinx_markdown_tables',
+]
 
 source_suffix = ['.rst', '.md']
 
